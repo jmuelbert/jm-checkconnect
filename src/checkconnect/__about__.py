b8ce1a43e700e5a2cf8acca51bf2abea1f08cc75
Jürgen Mülbert <jmuelbert@users.noreply.github.com>
2025-03-03 10:48:44 +0100

    chore: improve project

--- src/checkconnect/__about__.py
+++ src/checkconnect/__about__.py
@@ -1,5 +1,15 @@
-# # SPDX-License-Identifier: EUPL-1.2
+# SPDX-License-Identifier: EUPL-1.2
 #
 # SPDX-FileCopyrightText: © 2025-present Jürgen Mülbert
 #
+"""
+Metadata about the CheckConnect package.
+
+This module defines the version number of the CheckConnect package.
+"""
+
 __version__ = "0.0.1"
+"""
+The version number of the CheckConnect package.
+"""
+
