3e43adafa11b06b60013eff2f8a3081168f38b2f
Jürgen Mülbert <juergen.muelbert@outlook.de>
2025-06-07 09:31:47 +0200

    feat: Add project constants

--- src/checkconnect/__about__.py
+++ src/checkconnect/__about__.py
@@ -8,7 +8,16 @@ Metadata about the CheckConnect package.
 This module defines the version number of the CheckConnect package.
 """
 
-__version__ = "0.0.1"
+from __future__ import annotations
+
+from typing import Final
+
+__app_name__: Final[str] = "CheckConnect"
+__app_author__: Final[str] = "Jürgen Mülbert"
+__app_org_id__: Final[str] = "jmuelbert"
+__app_config_name__: Final[str] = "config.toml"
+
 """
 The version number of the CheckConnect package.
 """
+__version__ = "0.0.1"
