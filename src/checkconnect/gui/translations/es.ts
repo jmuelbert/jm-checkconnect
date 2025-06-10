ce2fe844792a69bdd79431243efb8581da5f81d7
Jürgen Mülbert <juergen.muelbert@outlook.de>
2025-06-10 12:43:16 +0200

    feat: extend translations. add many languages.

--- src/checkconnect/gui/translations/es.ts
+++ src/checkconnect/gui/translations/es.ts
@@ -4,121 +4,121 @@
 <context>
     <name>CheckConnectGUI</name>
     <message>
-        <location filename="../checkconnect_gui.py" line="158"/>
+        <location filename="../checkconnect_gui.py" line="145"/>
         <source>CheckConnect GUI - Version {__about__.__version__}</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="166"/>
+        <location filename="../checkconnect_gui.py" line="153"/>
         <source>Select NTP CSV File:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="168"/>
-        <location filename="../checkconnect_gui.py" line="179"/>
+        <location filename="../checkconnect_gui.py" line="155"/>
+        <location filename="../checkconnect_gui.py" line="166"/>
         <source>Browse</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="177"/>
+        <location filename="../checkconnect_gui.py" line="164"/>
         <source>Select URL CSV File:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="188"/>
+        <location filename="../checkconnect_gui.py" line="175"/>
         <source>Test NTP</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="190"/>
+        <location filename="../checkconnect_gui.py" line="177"/>
         <source>Test URLs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="192"/>
+        <location filename="../checkconnect_gui.py" line="179"/>
         <source>Generate Report</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="194"/>
+        <location filename="../checkconnect_gui.py" line="181"/>
         <source>Exit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="216"/>
+        <location filename="../checkconnect_gui.py" line="203"/>
         <source>Select NTP CSV File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="227"/>
+        <location filename="../checkconnect_gui.py" line="214"/>
         <source>Select URL CSV File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="244"/>
-        <location filename="../checkconnect_gui.py" line="267"/>
-        <location filename="../checkconnect_gui.py" line="297"/>
-        <location filename="../checkconnect_gui.py" line="314"/>
+        <location filename="../checkconnect_gui.py" line="231"/>
+        <location filename="../checkconnect_gui.py" line="254"/>
+        <location filename="../checkconnect_gui.py" line="284"/>
+        <location filename="../checkconnect_gui.py" line="301"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="245"/>
+        <location filename="../checkconnect_gui.py" line="232"/>
         <source>Invalid or missing NTP file selected.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="249"/>
+        <location filename="../checkconnect_gui.py" line="236"/>
         <source>Running NTP tests...
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="253"/>
+        <location filename="../checkconnect_gui.py" line="240"/>
         <source>NTP tests completed.
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="268"/>
+        <location filename="../checkconnect_gui.py" line="255"/>
         <source>Invalid or missing URL file selected.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="272"/>
+        <location filename="../checkconnect_gui.py" line="259"/>
         <source>Running URL tests...
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="276"/>
+        <location filename="../checkconnect_gui.py" line="263"/>
         <source>URL tests completed.
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="298"/>
+        <location filename="../checkconnect_gui.py" line="285"/>
         <source>Invalid or missing files for report generation.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="307"/>
+        <location filename="../checkconnect_gui.py" line="294"/>
         <source>Success</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="308"/>
+        <location filename="../checkconnect_gui.py" line="295"/>
         <source>Reports generated successfully.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="310"/>
+        <location filename="../checkconnect_gui.py" line="297"/>
         <source>Reports generated successfully.
 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../checkconnect_gui.py" line="315"/>
+        <location filename="../checkconnect_gui.py" line="302"/>
         <source>Error generating reports: {e}</source>
         <translation type="unfinished"></translation>
     </message>
